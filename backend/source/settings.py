62b14c830d85a597893259b7ee9673acedb3eb76
Bektur <project.bekturtilepbek@gmail.com>
2026-02-23 22:08:39 +0600

    добавил Dockerfile, docker-compose.yml и Документацию API

--- backend/source/settings.py
+++ backend/source/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'drf_spectacular',
     'users',
     'webapp',
     'api'
@@ -123,3 +124,15 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+REST_FRAMEWORK = {
+    # Если у тебя уже есть этот словарь, просто добавь туда эту строчку:
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'SportsMan API',
+    'DESCRIPTION': 'API для интернет-магазина спортивного питания',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,
+}
